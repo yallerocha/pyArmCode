45e9f3815c93c48964e2cc3f43e489b50208db60
Yalle Rocha <yalle.silva@ccc.ufcg.edu.br>
2023-06-08 21:17:38 -0300

    Update PyArmAngles.py

--- PyArmAngles.py
+++ PyArmAngles.py
@@ -2,7 +2,7 @@ import math
 
 def main():
   pyArm = PyArmAngles()
-  angles = pyArm.getAngles(16, 8, 12)
+  angles = pyArm.getAngles(-8, 8, 8)
     
   print(f"Angulo Base: {angles[0]}")
   print(f"Angulo Junta 1: {angles[1]}")
@@ -26,8 +26,9 @@ class PyArmAngles:
     if(self.positionValidation(joint3_x, joint3_y)):
 
       angleB = self.angleBase(x, y)
+
+      angle1 = self.angleJoint1(joint3_x, joint3_y)
       angle2 = self.angleJoint2(joint3_x, joint3_y)
-      angle1 = self.angleJoint1(joint3_x, joint3_y, angle2)
       angle3 = self.angleJoint3(angle1, angle2)
 
       return angleB, angle1, angle2, angle3
@@ -43,16 +44,16 @@ class PyArmAngles:
     return angle
   
   # Funcao que calcula o angulo da junta 1
-  def angleJoint1(self, x, y, angle2):
-    cos = (y**2 - x**2 - math.hypot(x,y)**2) / (-2 * x * math.hypot(x,y))
-    angle1 = math.degrees(math.acos(cos)) + (180 - angle2) / 2
+  def angleJoint1(self, x, y):
+    rad = math.atan2(y, x) + math.acos((x**2 + y**2 + self.L1**2 - self.L2**2) / (2 * self.L1 * math.sqrt(x**2 + y**2)))
+    angle1 = math.degrees(rad)
 
     return angle1
   
   # Funcao que calcula o angulo da junta 2
   def angleJoint2(self, x, y):
     rad = math.acos((x**2 + y**2 - self.L1**2 - self.L2**2) / (2 * self.L1 * self.L2))
-    angle2 = 140 - math.degrees(rad)
+    angle2 = 180 - math.degrees(rad)
 
     return angle2
   
@@ -65,7 +66,7 @@ class PyArmAngles:
   # Funcao que verifica se é possivel alcancar a posicao recebida
   def positionValidation(self, x, y):
     dist = math.sqrt(x**2 + y**2)
-    
+
     if(dist <= self.L1 + self.L2):
       return True
     
@@ -73,10 +74,13 @@ class PyArmAngles:
     
   # Funcao que retorna o X e Y em relação a junta 3 para calcular os angulos
   def joint3Coords(self, x, y, z):
-    joint3_x = math.hypot(x, y) - self.L3 * math.cos(self.fi)
-    joint3_y = z - self.L3 * math.sin(self.fi)
+    if(y >= 0):
+      joint3_x = math.hypot(x, y) - self.L3 * math.cos(self.fi)
+      joint3_y = z - self.L3 * math.sin(self.fi)
 
-    return joint3_x, joint3_y
+      return joint3_x, joint3_y
+    else:
+      raise positionError("Position outside the workspace!")
   
 class positionError(Exception):
   pass
